63047152569e1c498cd24766b8ae70342a7a86de
Saida Davis <saihdavis@gmail.com>
2020-07-19 02:38:38 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,19 +1,26 @@
+require "pry" 
 def roll_call_dwarves(dwarf_names)
   dwarf_names.each_with_index do |name, index|
     index += 1
     puts "#{index}. #{name}"
 end
 end
-
-def summon_captain_planet# code an argument here
-  # Your code here
+#binding.pry 
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |planeteer_calls| planeteer_calls.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |calls|
+    calls.size > 4
 end
+end 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(string)
+  i = 0 
   cheese_types = ["cheddar", "gouda", "camembert"]
+  while i < cheese_types.length 
+  return cheese_types.first if string.include?(cheese_types[i])
+  i = i + 1
+end
 end
